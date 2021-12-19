b85b41fa519d7a9f6593bd0c0f270614f4aa101f
prosellen <prosellen@users.noreply.github.com>
2021-12-19 19:12:14 +0100

    Install and config `gatsby-source-magento2`

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,14 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Gatsby Magento Source Error",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-source-magento2",
+      options: {
+        // Demo instances taken from the interwebs. Other stores with products (and product images) should yield the same errors
+        // graphqlEndpoint: "https://magento2.demo.ubertheme.com/graphql",
+        graphqlEndpoint: "http://magento2.demo.ubertheme.com/graphql",
+      }
+    }
+  ],
 };
